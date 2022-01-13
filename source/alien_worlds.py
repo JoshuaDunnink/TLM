de2c7095af282f9d865b58f2ba7c4b9e9c454f6d
Joshua Dunnink <Joshua.Dunnink@ing.com>
2022-01-13 08:42:31 +0100

    more humanlike

--- source/alien_worlds.py
+++ source/alien_worlds.py
@@ -5,33 +5,47 @@ import geckodriver_autoinstaller
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import pyautogui
+from random import randint
 
-approve_position= (300, 550)
+approve_position = (300, 550)
 geckodriver_autoinstaller.install()
 
-user_agent = "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36"
-profile = webdriver.FirefoxProfile(
-    "/Users/FH13NF/Library/Application Support"
-    "/Firefox/Profiles/sux3dl6h.default-release"
-)
 
-profile.set_preference("dom.webdriver.enabled", False)
-profile.set_preference("useAutomationExtension", False)
-profile.set_preference("general.useragent.override", user_agent)
-profile.update_preferences()
-options = Options()
-# options.add_argument("--headless")
-desired = DesiredCapabilities.FIREFOX
+class Driver:
+    def __init__(self, profile=None):
+        self.user_agent = (
+            "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/"
+            "537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/"
+            "87.0.4280.141 Mobile Safari/537.36"
+        )
+        if not profile:
+            self.profile = webdriver.FirefoxProfile(
+                "/Users/FH13NF/Library/Application Support"
+                "/Firefox/Profiles/sux3dl6h.default-release"
+            )
+        else:
+            self.profile = profile
 
-driver = webdriver.Firefox(
-    firefox_profile=profile, desired_capabilities=desired, options=options
-)
+        self.profile.set_preference("dom.webdriver.enabled", False)
+        self.profile.set_preference("useAutomationExtension", False)
+        self.profile.set_preference(
+            "general.useragent.override", self.user_agent
+        )
+        self.profile.update_preferences()
+        self.options = Options()
+        # self.options.add_argument("--headless")
+        self.desired = DesiredCapabilities.FIREFOX
+        self.driver = webdriver.Firefox(
+            firefox_profile=self.profile,
+            desired_capabilities=self.desired,
+            options=self.options,
+        )
 
 
-def count_down():
+def count_down(driver):
     countdown = ""
     while not countdown:
-        countdown = driver.find_element_by_id("countdown").text
+        countdown = driver.driver.find_element_by_id("countdown").text
         sleep(1)
     print("current countdown: " + countdown)
 
@@ -42,32 +56,100 @@ def count_down():
         return (0, 0, 0)
 
 
-def mine():
-    driver.find_element_by_id("mine").click()
-    countdown = count_down()
+def click_mine():
+    # driver.driver.find_element_by_id("mine").click()
+    pyautogui.moveTo(80, 300, 0.5)
+    sleep(1)
+    pyautogui.click(80, 300)
+    sleep(1)
+    pyautogui.click(80, 300)
+
+
+def click_claim():
+    pyautogui.moveTo(80, 350, 0.5)
+    sleep(1)
+    pyautogui.click(80, 350)
+    sleep(1)
+    pyautogui.click(80, 350)
+    # driver.driver.find_element_by_id("claim").click()
+    sleep(2)
+
+
+def click_approve():
+    # driver.driver.switch_to.window(driver.driver.window_handles[1])
+    # if (
+    #     "Transaction Request" in driver.driver.page_source
+    #     and "mine" in driver.driver.page_source
+    # ):
+    #     approve = driver.driver.find_element_by_class_name("button-text")
+    #     if approve.text == "Approve":
+    #         approve.click()
+    #     else:
+    #         exit(1)
+    sleep(2)
+    pyautogui.moveTo(300, 550, 0.5)
+    sleep(2)
+    pyautogui.click(x=300, y=550)
+    sleep(1)
+    pyautogui.click(x=300, y=550)
+    sleep(2)
+    # driver.driver.switch_to.window(main_window)
+
+
+def move_mouse(speed=1, enabled=True):
+    if enabled:
+        width, height = pyautogui.size()
+        random_width = randint(0, width)
+        random_height = randint(0, height)
+        pyautogui.moveTo(random_width, random_height, 1 / speed)
+
+
+def mine(driver):
+    if len(driver.driver.window_handles) == 1:
+        click_mine()
+    countdown = count_down(driver)
     if countdown != (0, 0, 0):
         return countdown
     while countdown == (0, 0, 0):
-        driver.find_element_by_id("claim").click()
-        sleep(5)
-        pyautogui.click(x=300, y=550)
-        sleep(1)
-        pyautogui.click(x=300, y=550)
-        sleep(10)
-        countdown = count_down()
+        sleep(2)
+        move_mouse(4)
+        if len(driver.driver.window_handles) == 1:
+            click_mine()
+        sleep(2)
+        if len(driver.driver.window_handles) > 1:
+            click_approve()
+        else:
+            click_claim()
+        countdown = count_down(driver)
     return (0, 0, 0)
 
 
 def main():
+    driver = Driver()
+    driver.driver.get("https://play.pocketaliens.io/")
     while True:
-        driver.get("https://play.pocketaliens.io/")
-        while driver.find_element_by_id("loginresponse").text != "eqqba.wam":
-            driver.find_element_by_id("login").click()
+        move_mouse(4)
+        main_window = driver.driver.window_handles[0]
+        main_window_title = driver.driver.title
+        while (
+            driver.driver.find_element_by_id("loginresponse").text
+            != "eqqba.wam"
+        ):
+            driver.driver.find_element_by_id("login").click()
             sleep(5)
-
-        balance = driver.find_element_by_id("response").text
+        balance = driver.driver.find_element_by_id("balance").text
         print("current balance: " + balance)
-        count_down = mine()
+
+        move_mouse(4)
+        if len(driver.driver.window_handles) > 1:
+            click_approve()
+
+        move_mouse(4)
+        if not driver.driver.title == main_window_title:
+            driver.driver.switch_to.window(main_window)
+
+        move_mouse(4)
+        count_down = mine(driver)
         sleep_time = (
             count_down[0] * 3600 + count_down[1] * 60 + count_down[2] + 5
         )
@@ -76,11 +158,14 @@ def main():
         now = datetime.now()
         delta = now + timedelta(seconds=sleep_time)
         while now < delta:
+            if len(driver.driver.window_handles) > 1:
+                click_approve()
             sleep(60)
-            pyautogui.click(x=300, y=550)
+            move_mouse(4)
             sleep(1)
-            pyautogui.click(x=300, y=550)
+            move_mouse(4)
             now = datetime.now()
+        driver.driver.refresh()
 
 
 main()
